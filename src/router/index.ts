1eb7d3a4a91c64503451767f59466170bf47a103
André Canuto <andrecanutoc@gmail.com>
2026-02-09 11:43:34 -0300

    feat: new routes added

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,7 +1,18 @@
 import { createRouter, createWebHistory } from "vue-router";
 
-import DashboardView from "../views/Dashboard.vue";
-import MatchView from "../views/MatchSimulator.vue";
+import DashboardView from "@/views/Dashboard.vue";
+
+import TeamFormationView from '@/views/TeamSquad/TeamFormationView.vue'
+import TeamTrainingView from '@/views/TeamSquad/TeamTrainingView.vue'
+
+import StadiumView from '@/views/Team/StadiumView.vue'
+import TrophyRoomView from '@/views/Team/TrophyRoomView.vue'
+import PressRoomView from '@/views/Team/PressRoomView.vue'
+
+import FinancesView from '@/views/Finances/FinancesView.vue'
+import TransferMarketView from '@/views/TransferMarket/TransferMarketView.vue'
+
+import MatchView from "@/views/MatchSimulator.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -16,6 +27,33 @@ const router = createRouter({
     {
       path: "/",
       component: DashboardView,
+    },
+    {
+      path: "/elenco",
+      children: [
+        { path: 'formacao', component: TeamFormationView },
+        { path: 'treinamento', component: TeamTrainingView },
+      ]
+    },
+    {
+      path: "/clube",
+      children: [
+        { path: 'estadio', component: StadiumView },
+        { path: 'sala-trofeus', component: TrophyRoomView },
+        { path: 'imprensa', component: PressRoomView },
+      ]
+    },
+    {
+      path: "/financas",
+      children: [
+        { path: '', component: FinancesView },
+      ]
+    },
+    {
+      path: "/mercado",
+      children: [
+        { path: '', component: TransferMarketView },
+      ]
     },
     {
       path: "/match",
