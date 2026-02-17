c8e32604573d246973b05693ea54eccc9e62e07d
André Canuto <andrecanutoc@gmail.com>
2026-02-17 11:14:05 -0300

    feat: position tag component done

---
+++ src/components/atoms/PositionTag.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { Position } from "@/models/team";
+import { positionName } from "@constants/team";
+
+defineProps<{ position: Position }>();
+</script>
+
+<template>
+  <span
+    class="badge tooltip tooltip-bottom"
+    :data-tip="positionName[position]"
+    :class="{
+      'badge-error': position === 'FWD',
+      'badge-warning': position === 'MID',
+      'badge-info': position === 'DEF',
+      'badge-neutral': position === 'GK',
+    }"
+  >
+    {{ position }}
+  </span>
+</template>
