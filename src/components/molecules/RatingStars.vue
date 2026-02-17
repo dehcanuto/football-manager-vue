4eb48cd718aa04e904d27bfe65ec7aaf72578d24
André Canuto <andrecanutoc@gmail.com>
2026-02-17 11:40:01 -0300

    feat: rating stars component

---
+++ src/components/molecules/RatingStars.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+defineProps<{ max: number; level: number }>();
+</script>
+
+<template>
+  <div class="rating rating-sm pointer-events-none select-none gap-1">
+    <div
+      v-for="n in max"
+      :key="n"
+      class="mask mask-star w-4 h-4"
+      :class="n <= level ? 'bg-yellow-400' : 'bg-gray-700'"
+    ></div>
+  </div>
+</template>
