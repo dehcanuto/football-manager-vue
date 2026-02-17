c77c5cb23a41b5467c0f54b0df2069ac0ea626e8
André Canuto <andrecanutoc@gmail.com>
2026-02-17 11:14:23 -0300

    feat: stadium layout done

---
+++ src/components/atoms/StadiumLayout.vue
@@ -0,0 +1,72 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div id="stadium_layout">
+    <div class="absolute inset-2 border-2 border-white rounded-lg" />
+    <div
+      class="absolute left-2 right-2 top-1/2 -translate-y-1/2 h-[2px] bg-white"
+    />
+
+    <div
+      class="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 w-[90px] h-[90px] border-2 border-white rounded-full"
+    />
+    <div
+      class="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 w-[5px] h-[5px] bg-white rounded-full"
+    />
+
+    <div
+      class="absolute top-2 left-[20%] w-[60%] h-[75px] border-2 border-white"
+    />
+    <div
+      class="absolute top-2 left-[35%] w-[30%] h-[30px] border-2 border-white"
+    />
+    <div
+      class="absolute top-[55px] left-1/2 -translate-x-1/2 w-[5px] h-[5px] bg-white rounded-full"
+    />
+
+    <div
+      class="absolute top-[82px] left-1/2 -translate-x-1/2 w-[70px] h-[40px] overflow-hidden rotate-180"
+    >
+      <div
+        class="absolute left-0 bottom-0 w-full h-full border-2 border-white rounded-full translate-y-1/2"
+      />
+    </div>
+
+    <div
+      class="absolute bottom-2 left-[20%] w-[60%] h-[75px] border-2 border-white"
+    />
+    <div
+      class="absolute bottom-2 left-[35%] w-[30%] h-[30px] border-2 border-white"
+    />
+    <div
+      class="absolute bottom-[55px] left-1/2 -translate-x-1/2 w-[5px] h-[5px] bg-white rounded-full"
+    />
+
+    <div
+      class="absolute bottom-[82px] left-1/2 -translate-x-1/2 w-[70px] h-[40px] overflow-hidden"
+    >
+      <div
+        class="absolute left-0 bottom-0 w-full h-full border-2 border-white rounded-full translate-y-1/2"
+      />
+    </div>
+
+    <div
+      class="absolute left-2 top-2 w-6 h-6 border-t-2 border-l-2 border-white rounded-tl-full"
+    />
+    <div
+      class="absolute right-2 top-2 w-6 h-6 border-t-2 border-r-2 border-white rounded-tr-full"
+    />
+    <div
+      class="absolute left-2 bottom-2 w-6 h-6 border-b-2 border-l-2 border-white rounded-bl-full"
+    />
+    <div
+      class="absolute right-2 bottom-2 w-6 h-6 border-b-2 border-r-2 border-white rounded-br-full"
+    />
+    <div
+      class="absolute top-0 left-1/2 -translate-x-1/2 w-[90px] h-3 border-t-2 border-white"
+    />
+    <div
+      class="absolute bottom-0 left-1/2 -translate-x-1/2 w-[90px] h-3 border-b-2 border-white"
+    />
+  </div>
+</template>
